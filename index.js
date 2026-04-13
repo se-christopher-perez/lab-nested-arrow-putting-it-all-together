71298d34758ba78d34da6a0bfa46a00beb9c0c7a
Christopher Perez <Data.Christopher.Perez@Gmail.com>
2026-04-13 16:36:51 -0400

    updates

--- index.js
+++ index.js
@@ -1,4 +1,46 @@
 
+const readline = require("readline")
+
+const readlinecl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+const userInfo = {
+  user: "user1",
+  password: "password1234"
+}
+
+
+function createLoginTracker (password) {
+
+  let attemptCount = 0
+
+  const loginAttempt = (passwordAttempt) => {
+    
+    attemptCount += 1
+
+    if (attemptCount > 3) {
+
+      return "Locked"
+
+    }
+
+    if (password === passwordAttempt) {
+      
+      return "Login successful"
+    
+    } else {
+
+      "Login Failed"
+
+    }
+
+    return loginAttempt
+
+  }
+
+}
 
 
 module.exports = {
